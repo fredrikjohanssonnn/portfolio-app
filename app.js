bbd6e77829793274ac4b943688abe6c151b23b11
fredrikjohanssonnn <fjohansson.creative@gmail.com>
2020-07-24 19:48:21 +0200

    Added project details to the /project/id url

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 /* Require dependencies and initiate the application */
 const express = require('express');
 const cookieParser = require('cookie-parser');
-const indexRouter = require('./routes/index');
 const path = require('path');
+
+const port = 3000;
 const app = express();
 
 /* Define a view engine and where the views folder is located */
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use('/', indexRouter);
+/* Use a route called /static when locating folder/files inside the public dir */
+app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use(require('./routes'));
 
-app.listen(3000);
+app.listen(port, console.log(`The app is live and listens to port ${port}`));
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const { projects } = require('../data.json');
 
 /* Get the landing page when no path is entered */
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/about', (req, res) => {
+  res.render('about', { title: 'Express' });
+});
+
+router.get('/project/:id', (req, res) => {
+  const { id } = req.params;
+  const details = projects[id];
+  const { project_name, description, technologies } = details;
+  res.render('project', details);
 });
 
 module.exports = router;
